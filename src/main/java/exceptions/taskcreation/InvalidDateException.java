442248193d01a4e1d848acece50bbdcca3254fe0
LonelyFort <jeffliu5925b@gmail.com>
2025-01-24 00:07:04 +0800

    Added javadoc for utility class DateTimeValueHandler and InvalidDateException

--- src/main/java/exceptions/taskcreation/InvalidDateException.java
+++ src/main/java/exceptions/taskcreation/InvalidDateException.java
@@ -1,7 +1,16 @@
 package exceptions.taskcreation;
 
+/**
+ * Exception thrown when an invalid date is provided during task creation.
+ */
 public class InvalidDateException extends TaskCreationException {
 
+    /**
+     * Returns a string representation of the exception.
+     * This includes the error message indicating the correct date and time syntax.
+     *
+     * @return a string representation of the exception
+     */
     @Override
     public String toString() {
         return "Invalid date. Please enter the date and time in syntax yyyy-mm-dd HH:mm";
